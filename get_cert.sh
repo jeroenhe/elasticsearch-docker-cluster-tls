9bb30c6bd8329a7757803298e835183ddf1ce37c
Jeroen Hendricksen <jeroen.hendricksen@nedap.com>
2022-03-14 09:12:23 +0100

    Use compose cp command

--- get_cert.sh
+++ get_cert.sh
@@ -2,4 +2,4 @@
 
 # Write the generated public key ca certificate to disk,
 # for usage from the command-line.
-docker-compose cp elasticsearch1:/usr/share/elasticsearch/config/certs/ca/ca.crt .
+docker compose cp elasticsearch1:/usr/share/elasticsearch/config/certs/ca/ca.crt .
